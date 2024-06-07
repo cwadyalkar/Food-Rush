892804bb2bf3572b0655463b6be8329cca93e932
Chirag Wadyalkar <cwadyalkar@gmail.com>
2024-06-07 15:10:06 +0530

    updated string

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 export const connectDB = async()=>{
-    await mongoose.connect('mongodb://localhost:27017/food-delivery').then(()=>{
+    await mongoose.connect('mongodb+srv://chirag:9834625550@chiragdb.6rexbsk.mongodb.net/food-delivery').then(()=>{
         console.log("DB is Connected")
     })
 } 
