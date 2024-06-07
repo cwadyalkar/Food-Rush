52b8719117c3a91c0cee818ad0348a540ca65a44
Chirag Wadyalkar <cwadyalkar@gmail.com>
2024-06-07 11:38:16 +0530

    updted pjson

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,32 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-import "dotenv/config.js"
+import "dotenv/config.js";
+
 import cartRouter from "./routes/cartRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
 
-
-
-
-const app = express()
+const app = express();
 const PORT = 4000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 connectDB();
 
-app.use("/api/food",foodRouter)
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
-app.use("/images",express.static('uploads'))
+app.use("/api/food", foodRouter);
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+app.use("/images", express.static("uploads"));
+/////////
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
+////////
 
-app.listen(PORT,()=>{
-    console.log(`App is Listening to the Port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App is Listening to the Port ${PORT}`);
+});
 
 // mongodb+srv://cwadyalkar:<password>@cluster0.e51vk0x.mongodb.net/?
